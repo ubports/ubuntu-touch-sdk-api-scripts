a934cf3aa82e74f3ff50179282c65c4ae34a5396
Michael Hall <mhall119@ubuntu.com>
2014-11-24 15:50:21 -0500

    Add project dir to PYTHONPATH

--- developer_portal/wsgi.py
+++ developer_portal/wsgi.py
@@ -7,8 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local_settings")
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+os.environ["DJANGO_SETTINGS_MODULE"] = "local_settings"
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
