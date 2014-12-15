08c9fe197f3a64b1b2387d9f15cf87d55694760e
Daniel Holbach <daniel.holbach@canonical.com>
2014-12-15 15:57:49 +0100

    From https://docs.djangoproject.com/en/1.6/topics/i18n/translation/:
    It should come after SessionMiddleware, because LocaleMiddleware makes use 
    of session data. And it should come before CommonMiddleware because 
    CommonMiddleware needs an activated language in order to resolve the 
    requested URL.

--- developer_portal/settings.py
+++ developer_portal/settings.py
@@ -75,12 +75,12 @@ INSTALLED_APPS = [
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
 
     'cms.middleware.user.CurrentUserMiddleware',
     'cms.middleware.page.CurrentPageMiddleware',
