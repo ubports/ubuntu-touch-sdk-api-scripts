38064b282bace6e16593b5fdff6a77c86f5a5b11
Michael Hall <mhall119@ubuntu.com>
2015-01-15 12:45:40 -0500

    Add Zinnia urlconf outside of django-cms's paths to avoid conflicts between the two

--- developer_portal/urls.py
+++ developer_portal/urls.py
@@ -26,7 +26,10 @@ listen_for_login()
 urlpatterns += i18n_patterns('',
     url(r'^search/', 'developer_portal.views.search', name='search'),
     url(r'^ckeditor/', include('ckeditor.urls')),
-    url(r'^community/blog/', include('developer_portal.blog.urls', namespace='zinnia')),
-    url(r'^community/blog/comments/', include('django_comments.urls')),
     url(r'^', include('cms.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += patterns('',
+    url(r'^blog/', include('zinnia.urls', namespace='zinnia')),
+    url(r'^blog/comments/', include('django_comments.urls')),
+)
