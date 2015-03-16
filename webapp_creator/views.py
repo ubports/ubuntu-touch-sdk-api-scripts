f777f9051493921bca46972081e4da6990158d61
David Calle <davidc@framli.eu>
2015-03-16 08:43:44 +0100

    Login decorator in views.py, review comment fixes

--- webapp_creator/views.py
+++ webapp_creator/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from django import forms
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.decorators import login_required
 import woc
 
 APP_OPTIONS = (
@@ -17,31 +18,32 @@ APP_OPTIONS = (
 class WebappForm(forms.Form):
     displayname = forms.CharField(
         max_length=200, required=True, label=_('App name'),
-        help_text=_('eg. Duck Duck Go'))
+        help_text=_('E.g. Duck Duck Go'))
     url = forms.URLField(
         max_length=200, required=True, label=_('Webapp URL'),
-        help_text=_('eg. https://duckduckgo.com'))
+        help_text=_('E.g. https://duckduckgo.com'))
     icon = forms.FileField(
         required=True, label=_('App icon'),
-        help_text=_('Recommended 256x256 px, png format'))
+        help_text=_('Recommended 256x256 px, PNG format'))
     options = forms.MultipleChoiceField(
         choices=APP_OPTIONS,
-        label=_("App options"), help_text=_('Use CTRL to select multiple options'),
+        label=_("App options"),
+        help_text=_('Use CTRL to select multiple options'),
         required=False)
     nickname = forms.RegexField(
         regex='^[\w-]+$', max_length=200, required=True,
         label=_('Developer namespace'),
         help_text=_('The namespace you picked for your\
             <a href="https://myapps.developer.ubuntu.com/dev/account/">\
-            MyApps account</a>'))
+            MyApps account</a>. E.g. miaotian'))
     fullname = forms.CharField(
         max_length=200, required=True, label=_('Maintainer full name'),
-        help_text=_('eg. Miao Tian'))
+        help_text=_('E.g. Miao Tian'))
     email = forms.EmailField(
-        max_length=200, required=True, label=_('Maintainer email'),
-        help_text=_('eg. miaotian@ubuntu.com'))
+        max_length=200, required=True, label=_('Maintainer e-mail'),
+        help_text=_('E.g. miaotian@ubuntu.com'))
 
-
+@login_required
 def webapp(request):
     if request.method == 'POST':
         webapp_form = WebappForm(request.POST, request.FILES)
