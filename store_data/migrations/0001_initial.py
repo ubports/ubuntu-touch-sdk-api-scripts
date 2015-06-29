a90526dfb44ec8347ab25c06df81ab40a8daab4d
Daniel Holbach <daniel.holbach@canonical.com>
2015-06-29 12:27:16 +0200

    update migrations to use CMSPlugin instead of standard model

--- store_data/migrations/0001_initial.py
+++ store_data/migrations/0001_initial.py
@@ -10,21 +10,21 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
         # Adding model 'Release'
         db.create_table(u'store_data_release', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=25)),
         ))
         db.send_create_signal(u'store_data', ['Release'])
 
         # Adding model 'Architecture'
         db.create_table(u'store_data_architecture', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=10)),
         ))
         db.send_create_signal(u'store_data', ['Architecture'])
 
         # Adding model 'GadgetSnap'
         db.create_table(u'store_data_gadgetsnap', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'cmsplugin_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.CMSPlugin'], unique=True, primary_key=True)),
             ('icon_url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('ratings_average', self.gf('django.db.models.fields.DecimalField')(max_digits=2, decimal_places=1)),
@@ -74,17 +74,38 @@ class Migration(SchemaMigration):
 
 
     models = {
-        u'store_data.architecture': {
-            'Meta': {'object_name': 'Architecture'},
+        'cms.cmsplugin': {
+            'Meta': {'object_name': 'CMSPlugin'},
+            'changed_date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '15', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.CMSPlugin']", 'null': 'True', 'blank': 'True'}),
+            'placeholder': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Placeholder']", 'null': 'True'}),
+            'plugin_type': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'}),
+            'position': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+        },
+        'cms.placeholder': {
+            'Meta': {'object_name': 'Placeholder'},
+            'default_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slot': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'store_data.architecture': {
+            'Meta': {'object_name': 'Architecture', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '10'})
         },
         u'store_data.gadgetsnap': {
-            'Meta': {'object_name': 'GadgetSnap'},
+            'Meta': {'object_name': 'GadgetSnap', '_ormbases': ['cms.CMSPlugin']},
             'alias': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'architecture': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['store_data.Architecture']", 'symmetrical': 'False'}),
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
             'icon_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_updated': ('django.db.models.fields.DateTimeField', [], {}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '5', 'decimal_places': '2'}),
@@ -95,8 +116,8 @@ class Migration(SchemaMigration):
             'version': ('django.db.models.fields.CharField', [], {'max_length': '25'})
         },
         u'store_data.release': {
-            'Meta': {'object_name': 'Release'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'Meta': {'object_name': 'Release', '_ormbases': ['cms.CMSPlugin']},
+            u'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '25'})
         }
     }
