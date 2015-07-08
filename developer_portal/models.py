e544ad37a1ffa72d63fe8d70befb424390127d37
Daniel Holbach <daniel.holbach@canonical.com>
2015-07-08 14:35:51 +0200

    add table to store which snappy docs branches we are tracking and where

--- developer_portal/models.py
+++ developer_portal/models.py
@@ -1,3 +1,5 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from cms.models import CMSPlugin
 from djangocms_text_ckeditor.html import extract_images
@@ -13,3 +15,11 @@ class RawHtml(AbstractText):
         body = extract_images(body, self)
         self.body = body
         AbstractText.save(self, *args, **kwargs)
+
+
+class SnappyDocsBranch(models.Model):
+    branch_origin = models.CharField(max_length=200,
+        help_text=_('Launchpad branch location, ie: lp:snappy/15.04'))
+    path_alias = models.CharField(max_length=20,
+        help_text=_('Path alias we want to use for the docs, '
+                    'ie "15.04" or "latest", etc.'))
