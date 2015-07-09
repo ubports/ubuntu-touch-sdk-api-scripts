03c0fb64ae318e34dfd4749d1336b488454afc93
Daniel Holbach <daniel.holbach@canonical.com>
2015-07-09 16:49:50 +0200

    integrate https://code.launchpad.net/~ubuntudeveloperportal-editors/+junk/snappy-docs

--- developer_portal/management/commands/import-snappy-branches.py
+++ developer_portal/management/commands/import-snappy-branches.py
@@ -1,16 +1,32 @@
 from django.core.management.base import NoArgsCommand
 
+from bs4 import BeautifulSoup
+import codecs
 import glob
 import logging
+import markdown
 import os
+import re
+import requests
 import shutil
 import subprocess
 import tempfile
 
 from developer_portal.models import SnappyDocsBranch
 
+MAP = {"autopilot.md": None,
+       "config.md": "config-command",
+       "cross-build.md": None,
+       "frameworks.md": "frameworks",
+       "garbage.md": None,
+       "hashes.md": None,
+       "meta.md": "package-metadata",
+       "oem.md": "oem",
+       "package-names.md": None,
+       "security.md": "security-policy"}
 
-def import_branch(origin, alias):
+
+def get_branch_from_lp(origin, alias):
     return subprocess.call([
         'bzr', 'checkout', '--lightweight', origin, alias])
 
@@ -19,8 +35,53 @@ def clean_up(directory):
     shutil.rmtree(directory)
 
 
-def import_markdown(local_branch):
-    print(local_branch)
+def get_published_titles():
+    titles = {}
+    for m in MAP:
+        if MAP.get(m, False):
+            url = "https://developer.ubuntu.com/en/snappy/guides/%s" % MAP[m]
+            r = requests.get(url)
+            soup = BeautifulSoup(r.text)
+            titles[m] = soup.title.string.split(" | ")[0]
+    return titles
+
+
+class LocalBranch():
+    def __init__(self, dirname, titles):
+        self.dirname = dirname
+        self.docs_path = os.path.join(self.dirname, 'docs')
+        self.doc_fns = glob.glob(self.docs_path+'/*.md')
+        self.titles = titles
+
+    def import_markdown(self):
+        for doc_fn in self.doc_fns:
+            html = self.md_to_html(doc_fn)
+            html = self.replace_links(html)
+            html_fn = doc_fn.replace('.md', '.html')
+            with open(html_fn, "w") as t:
+                t.write(html)
+
+    def replace_links(self, doc):
+        for t in self.titles:
+            url = u"https://developer.ubuntu.com/en/snappy/guides/%s" % MAP[t]
+            link = u"<a href=\"%s\">%s</a>" % (url, self.titles[t])
+            doc = unicode(doc.encode('utf-8'))
+            doc = doc.replace(t, link)
+        doc = re.sub(r"<html>\n\s<body>\n", "", doc, flags=re.MULTILINE)
+        doc = re.sub(r"\s<\/body>\n<\/html>", "", doc, flags=re.MULTILINE)
+        return doc
+
+    def md_to_html(self, doc_fn):
+        with codecs.open(doc_fn, 'r', encoding='utf-8') as f:
+            html = markdown.markdown(f.read(), output_format="html5")
+        begin = "<div class=\"row no-border\">\n<div class=\"eight-col\">\n"
+        end = "</div>\n</div>"
+        html = begin + html + end
+        html = html.replace("<pre><code>",
+                            "</div><div class=\"twelve-col\"><pre><code>")
+        html = html.replace("</code></pre>",
+                            "</code></pre></div><div class=\"eight-col\">")
+        return html
 
 
 def import_branches():
@@ -32,15 +93,17 @@ def import_branches():
     pwd = os.getcwd()
     os.chdir(tempdir)
     for branch in SnappyDocsBranch.objects.all():
-        if import_branch(branch.branch_origin, branch.path_alias) != 0:
+        if get_branch_from_lp(branch.branch_origin, branch.path_alias) != 0:
             logging.error(
                 'Could not check out branch "%s".' % branch.branch_origin)
             shutil.rmtree(os.path.join(tempdir, branch.path_alias))
             break
+    titles = get_published_titles()
     os.chdir(pwd)
     for local_branch in [a for a in glob.glob(tempdir+'/*')
                          if os.path.isdir(a)]:
-        import_markdown(local_branch)
+        branch = LocalBranch(local_branch, titles)
+        branch.import_markdown()
     shutil.rmtree(tempdir)
 
 
