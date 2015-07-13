024647c03a8a94970bcc8eee38cc2405924e66c7
Daniel Holbach <daniel.holbach@canonical.com>
2015-07-13 17:22:45 +0200

    split functionality related to replacing links into its own class, remove the requirement to rely on developer.u.c

--- developer_portal/management/commands/import-snappy-branches.py
+++ developer_portal/management/commands/import-snappy-branches.py
@@ -7,23 +7,53 @@ import logging
 import markdown
 import os
 import re
-import requests
 import shutil
 import subprocess
 import tempfile
 
 from developer_portal.models import SnappyDocsBranch
 
-MAP = {u"autopilot.md": None,
-       u"config.md": u"config-command",
-       u"cross-build.md": None,
-       u"frameworks.md": u"frameworks",
-       u"garbage.md": None,
-       u"hashes.md": None,
-       u"meta.md": u"package-metadata",
-       u"oem.md": u"oem",
-       u"package-names.md": None,
-       u"security.md": u"security-policy"}
+
+class MarkdownFile():
+    html = None
+
+    def __init__(self, fn):
+        self.fn = fn
+        self.html_fn = self.fn.replace('.md', '.html')
+        with codecs.open(self.fn, 'r', encoding='utf-8') as f:
+            self.html = markdown.markdown(f.read(), output_format="html5")
+        self.title = self._read_title()
+        self._remove_body_and_html_tags()
+        self._use_developer_site_style()
+
+    def _read_title(self):
+        soup = BeautifulSoup(self.html)
+        if soup.title:
+            return soup.title.text
+        if soup.h1:
+            return soup.h1.text
+        return os.path.basename(self.fn).replace('-', ' ').title()
+
+    def _remove_body_and_html_tags(self):
+        self.html = re.sub(r"<html>\n\s<body>\n", "", self.html,
+                           flags=re.MULTILINE)
+        self.html = re.sub(r"\s<\/body>\n<\/html>", "", self.html,
+                           flags=re.MULTILINE)
+
+    def _use_developer_site_style(self):
+        begin = u"<div class=\"row no-border\">\n<div class=\"eight-col\">\n"
+        end = u"</div>\n</div>"
+        self.html = begin + self.html + end
+        self.html = self.html.replace(
+            "<pre><code>",
+            "</div><div class=\"twelve-col\"><pre><code>")
+        self.html = self.html.replace(
+            "</code></pre>",
+            "</code></pre></div><div class=\"eight-col\">")
+
+    def save(self):
+        with codecs.open(self.html_fn, "w", encoding='utf-8') as t:
+            t.write(self.html)
 
 
 def get_branch_from_lp(origin, alias):
@@ -31,57 +61,31 @@ def get_branch_from_lp(origin, alias):
         'bzr', 'checkout', '--lightweight', origin, alias])
 
 
-def clean_up(directory):
-    shutil.rmtree(directory)
-
-
-def get_published_titles():
-    titles = {}
-    for m in MAP:
-        if MAP.get(m, False):
-            url = "https://developer.ubuntu.com/en/snappy/guides/%s" % MAP[m]
-            r = requests.get(url)
-            soup = BeautifulSoup(r.text)
-            titles[m] = soup.title.string.split(" | ")[0]
-    return titles
-
-
 class LocalBranch():
-    def __init__(self, dirname, titles):
+    titles = {}
+
+    def __init__(self, dirname):
         self.dirname = dirname
         self.docs_path = os.path.join(self.dirname, 'docs')
         self.doc_fns = glob.glob(self.docs_path+'/*.md')
-        self.titles = titles
+        self.md_files = []
+
+    def _replace_links(self):
+        for md_file in self.md_files:
+            for title in self.titles:
+                url = u"https://developer.ubuntu.com/en/snappy/guides/%s" % \
+                    os.path.basename(md_file.html_fn).replace('.html', '')
+                link = u"<a href=\"%s\">%s</a>" % (url, self.titles[md_file.fn])
+                md_file.html = md_file.html.replace(os.path.basename(md_file.fn), link)
 
     def import_markdown(self):
         for doc_fn in self.doc_fns:
-            html = self.md_to_html(doc_fn)
-            html = self.replace_links(html)
-            html_fn = doc_fn.replace('.md', '.html')
-            with codecs.open(html_fn, "w", encoding='utf-8') as t:
-                t.write(html)
-
-    def replace_links(self, doc):
-        for t in self.titles:
-            url = u"https://developer.ubuntu.com/en/snappy/guides/%s" % MAP[t]
-            link = u"<a href=\"%s\">%s</a>" % (url, self.titles[t])
-            doc = unicode(doc)
-            doc = doc.replace(t, link)
-        doc = re.sub(r"<html>\n\s<body>\n", "", doc, flags=re.MULTILINE)
-        doc = re.sub(r"\s<\/body>\n<\/html>", "", doc, flags=re.MULTILINE)
-        return doc
-
-    def md_to_html(self, doc_fn):
-        with codecs.open(doc_fn, 'r', encoding='utf-8') as f:
-            html = markdown.markdown(f.read(), output_format="html5")
-        begin = u"<div class=\"row no-border\">\n<div class=\"eight-col\">\n"
-        end = u"</div>\n</div>"
-        html = begin + html + end
-        html = html.replace("<pre><code>",
-                            "</div><div class=\"twelve-col\"><pre><code>")
-        html = html.replace("</code></pre>",
-                            "</code></pre></div><div class=\"eight-col\">")
-        return html
+            md_file = MarkdownFile(doc_fn)
+            self.md_files += [md_file]
+            self.titles[md_file.fn] = md_file.title
+        self._replace_links()
+        for md_file in self.md_files:
+            md_file.save()
 
 
 def import_branches():
@@ -98,11 +102,10 @@ def import_branches():
                 'Could not check out branch "%s".' % branch.branch_origin)
             shutil.rmtree(os.path.join(tempdir, branch.path_alias))
             break
-    titles = get_published_titles()
     os.chdir(pwd)
     for local_branch in [a for a in glob.glob(tempdir+'/*')
                          if os.path.isdir(a)]:
-        branch = LocalBranch(local_branch, titles)
+        branch = LocalBranch(local_branch)
         branch.import_markdown()
     shutil.rmtree(tempdir)
 
