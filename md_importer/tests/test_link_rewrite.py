5f86bdc308983a326ded66cc67cc625cbd702304
Daniel Holbach <daniel.holbach@canonical.com>
2016-01-08 18:14:32 +0100

    add new test to check links in snapcraft import, move link checking function into TestLocalBranchImport

--- md_importer/tests/test_link_rewrite.py
+++ md_importer/tests/test_link_rewrite.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 
-from django.http.response import HttpResponseNotFound
-from django.test import Client
-
+from cms.api import publish_pages
 from cms.models import Page
 
 from md_importer.importer.article import Article
-from .utils import TestLocalBranchImport
+from .utils import (
+    db_add_empty_page,
+    TestLocalBranchImport,
+)
 
 
 class TestLinkRewrite(TestLocalBranchImport):
@@ -17,14 +18,36 @@ class TestLinkRewrite(TestLocalBranchImport):
         self.assertTrue(self.repo.publish())
         pages = Page.objects.all()
         self.assertEqual(pages.count(), 1+2)  # Home + 2 articles
-        c = Client()
         for article in self.repo.imported_articles:
             self.assertTrue(isinstance(article, Article))
             soup = BeautifulSoup(article.html, 'html5lib')
             for link in soup.find_all('a'):
                 if not link.has_attr('class') or \
                    'headeranchor-link' not in link.attrs['class']:
-                    res = c.get('/{}/'.format(link.attrs['href']))
-                    self.assertNotIsInstance(
-                        res, HttpResponseNotFound,
-                        msg='Link {} not found.'.format(link.attrs['href']))
+                    self.check_local_link(link.attrs['href'], pages)
+
+
+class TestSnapcraftLinkRewrite(TestLocalBranchImport):
+    def runTest(self):
+        self.create_repo('data/snapcraft-test')
+        snappy_page = db_add_empty_page('Snappy', self.home)
+        build_apps = db_add_empty_page('Build Apps', snappy_page)
+        publish_pages([snappy_page, build_apps])
+        self.assertEqual(
+            3, Page.objects.filter(publisher_is_draft=True).count())
+        self.repo.add_directive('docs', 'snappy/build-apps/devel')
+        self.repo.add_directive('README.md', 'snappy/build-apps/devel')
+        self.repo.add_directive(
+            'HACKING.md', 'snappy/build-apps/devel/hacking')
+        self.assertTrue(self.repo.execute_import_directives())
+        self.assertTrue(self.repo.publish())
+        pages = Page.objects.all()
+        for article in self.repo.imported_articles:
+            self.assertTrue(isinstance(article, Article))
+            self.assertGreater(len(article.html), 0)
+        for article in self.repo.imported_articles:
+            soup = BeautifulSoup(article.html, 'html5lib')
+            for link in soup.find_all('a'):
+                if not link.has_attr('class') or \
+                   'headeranchor-link' not in link.attrs['class']:
+                    self.check_local_link(link.attrs['href'], pages)
--- md_importer/tests/utils.py
+++ md_importer/tests/utils.py
@@ -1,8 +1,10 @@
 import os
 import shutil
+import sys
 import tempfile
 
-from django.test import TestCase
+from django.http.response import HttpResponseNotFound
+from django.test import Client, TestCase
 from django.utils.text import slugify
 
 from cms.api import create_page, publish_pages
@@ -10,6 +12,11 @@ from cms.models import Page
 
 from ..importer import DEFAULT_LANG
 from ..importer.repo import create_repo
+
+if sys.version_info.major == 2:
+    from urlparse import urlparse
+else:
+    from urllib.parse import urlparse
 
 
 def db_empty_page_list():
@@ -37,6 +44,7 @@ class TestLocalBranchImport(TestCase):
         self.assertEqual(len(Page.objects.filter(publisher_is_draft=False)), 0)
         self.assertEqual(len(Page.objects.filter(publisher_is_draft=True)), 1)
         self.assertEqual(Page.objects.all()[0].get_absolute_url(), '/en/')
+        self.client = Client()
 
     def create_repo(self, docs_path):
         self.tempdir = tempfile.mkdtemp()
@@ -45,6 +53,22 @@ class TestLocalBranchImport(TestCase):
         self.repo = create_repo(self.tempdir, origin, '', '')
         self.fetch_retcode = self.repo.get()
         self.assertEqual(self.fetch_retcode, 0)
+
+    def check_local_link(self, url, pages):
+        (scheme, netloc, path, params, query, fragment) = urlparse(url)
+        if scheme in ['http', 'https', 'mailto']:
+            # make external links pass
+            self.assertTrue(True)
+        else:
+            url = '/{}/'.format(url)
+            res = self.client.get(url)
+            self.assertNotIsInstance(
+                res, HttpResponseNotFound,
+                msg='Link {} not found. Available pages: {}'.format(
+                    url, ', '.join([p.get_absolute_url() for p in pages])))
+            self.assertGreater(
+                len(res.content), 0,
+                msg='Page {} is empty.'.format(url))
 
     def tearDown(self):
         shutil.rmtree(self.tempdir)
