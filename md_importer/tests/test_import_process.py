c3c84b4e28378fe37bf0af3f14cf740f7511f60c
Daniel Holbach <daniel.holbach@canonical.com>
2016-04-05 14:07:48 +0200

    add test

--- md_importer/tests/test_import_process.py
+++ md_importer/tests/test_import_process.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from cms.api import publish_pages
 from cms.models import Page
 
+from md_importer.importer import DEFAULT_LANG
 from md_importer.importer.process import process_branch
 from md_importer.models import (
     ExternalDocsBranch,
@@ -66,3 +67,57 @@ class TestImportProcessBranchWhichChangesFiles(TestCase):
         self.assertTrue(process_branch(branch))
         self.assertEqual(
             Page.objects.filter(publisher_is_draft=False).count(), 3)
+
+
+class TestImportProcessTwice(TestCase):
+    '''
+    This is a pretty standard snapcraft import. snappy/build-apps are
+    already created. Running the import twice used to create problems,
+    because the snappy/build-apps used to be deleted at the end of
+    process.process_branch. This test will make sure this will never
+    happen again.
+    '''
+    def runTest(self):
+        db_empty_page_list()
+        root = db_create_root_page()
+        snappy_page = db_add_empty_page('Snappy', root)
+        build_apps = db_add_empty_page(
+            'Build Apps', snappy_page, slug='build-apps')
+        publish_pages([snappy_page, build_apps])
+        ExternalDocsBranch.objects.all().delete()
+        ExternalDocsBranchImportDirective.objects.all().delete()
+        ImportedArticle.objects.all().delete()
+        branch, created = ExternalDocsBranch.objects.get_or_create(
+            origin=os.path.join(
+                os.path.dirname(__file__), 'data/snapcraft-test'),
+            branch_name='')
+        a, created = ExternalDocsBranchImportDirective.objects.get_or_create(
+            import_from='docs/intro.md', write_to='snappy/build-apps',
+            external_docs_branch=branch)
+        b, created = ExternalDocsBranchImportDirective.objects.get_or_create(
+            import_from='docs', write_to='snappy/build-apps',
+            external_docs_branch=branch)
+        repo = process_branch(branch)
+        self.assertIsNotNone(repo)
+        self.assertGreater(len(repo.pages), 3)
+        self.assertIn(
+            '/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in repo.pages])
+        published_pages = Page.objects.filter(publisher_is_draft=False)
+        self.assertIn(
+            '/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in published_pages])
+        self.assertGreater(published_pages.count(), 3)
+
+        # Run the import a second time
+        repo = process_branch(branch)
+        self.assertIsNotNone(repo)
+        self.assertGreater(len(repo.pages), 3)
+        self.assertIn(
+            '/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in repo.pages])
+        published_pages = Page.objects.filter(publisher_is_draft=False)
+        self.assertIn(
+            '/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in published_pages])
+        self.assertGreater(published_pages.count(), 3)
