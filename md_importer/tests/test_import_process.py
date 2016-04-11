c34e5c4ae9202411a6d4095e3163aeabe7f8bc8e
Daniel Holbach <daniel.holbach@canonical.com>
2016-04-11 15:55:33 +0200

    add second variant of whole import

--- md_importer/tests/test_import_process.py
+++ md_importer/tests/test_import_process.py
@@ -156,7 +156,7 @@ class TestImportProcessTwice(TestCase):
             self.assertFalse(imported_article.page.publisher_is_draft)
 
 
-class TestWholeImportProcessTwice(TestCase):
+class TestWholeImportProcessTwiceVariantA(TestCase):
     '''
     This is a pretty standard snapcraft import. snappy/build-apps are
     already created. Running the import twice used to create problems,
@@ -177,6 +177,98 @@ class TestWholeImportProcessTwice(TestCase):
         publish_pages([snappy_page, build_apps, guides, phone])
         self.assertEqual(
             Page.objects.filter(publisher_is_draft=False).count(), 5)
+        ExternalDocsBranch.objects.all().delete()
+        ExternalDocsBranchImportDirective.objects.all().delete()
+        ImportedArticle.objects.all().delete()
+
+        # Snapcraft import definition
+        snapcraft_branch, created = ExternalDocsBranch.objects.get_or_create(
+            origin=os.path.join(
+                os.path.dirname(__file__), 'data/snapcraft-test'),
+            branch_name='')
+        a, created = ExternalDocsBranchImportDirective.objects.get_or_create(
+            import_from='docs/intro.md', write_to='snappy/build-apps',
+            external_docs_branch=snapcraft_branch)
+        b, created = ExternalDocsBranchImportDirective.objects.get_or_create(
+            import_from='docs', write_to='snappy/build-apps',
+            external_docs_branch=snapcraft_branch)
+
+        # Snappy import definition
+        snappy_branch, created = ExternalDocsBranch.objects.get_or_create(
+            origin=os.path.join(
+                os.path.dirname(__file__), 'data/snappy-test'),
+            branch_name='')
+        c, created = ExternalDocsBranchImportDirective.objects.get_or_create(
+            import_from='docs', write_to='snappy/guides',
+            external_docs_branch=snappy_branch)
+
+        # Run the import a first time
+        snapcraft_repo = process_branch(snapcraft_branch)
+        self.assertIsNotNone(snapcraft_repo)
+        self.assertGreater(len(snapcraft_repo.pages), 10)
+        self.assertIn(
+            '/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in snapcraft_repo.pages])
+        snappy_repo = process_branch(snappy_branch)
+        self.assertIsNotNone(snappy_repo)
+        self.assertGreater(len(snappy_repo.pages), 10)
+        self.assertIn(
+            '/{}/snappy/guides/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in snappy_repo.pages])
+
+        published_pages = Page.objects.filter(publisher_is_draft=False)
+        published_urls = [p.get_absolute_url() for p in published_pages]
+        self.assertIn('/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+                      published_urls)
+        self.assertIn('/{}/snappy/guides/'.format(DEFAULT_LANG),
+                      published_urls)
+        self.assertGreater(published_pages.count(), 20)
+        for imported_article in ImportedArticle.objects.all():
+            self.assertFalse(imported_article.page.publisher_is_draft)
+
+        # Run the import a second time
+        snapcraft_repo = process_branch(snapcraft_branch)
+        self.assertIsNotNone(snapcraft_repo)
+        self.assertGreater(len(snapcraft_repo.pages), 10)
+        self.assertIn(
+            '/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in snapcraft_repo.pages])
+        snappy_repo = process_branch(snappy_branch)
+        self.assertIsNotNone(snappy_repo)
+        self.assertGreater(len(snappy_repo.pages), 10)
+        self.assertIn(
+            '/{}/snappy/guides/'.format(DEFAULT_LANG),
+            [p.get_absolute_url() for p in snappy_repo.pages])
+
+        published_pages = Page.objects.filter(publisher_is_draft=False)
+        published_urls = [p.get_absolute_url() for p in published_pages]
+        self.assertIn('/{}/snappy/build-apps/'.format(DEFAULT_LANG),
+                      published_urls)
+        self.assertIn('/{}/snappy/guides/'.format(DEFAULT_LANG),
+                      published_urls)
+        self.assertGreater(published_pages.count(), 20)
+        for imported_article in ImportedArticle.objects.all():
+            self.assertFalse(imported_article.page.publisher_is_draft)
+
+
+class TestWholeImportProcessTwiceVariantB(TestCase):
+    '''
+    This is a pretty standard snapcraft import. snappy/build-apps are
+    already created. Running the import twice used to create problems,
+    because the snappy/build-apps used to be deleted at the end of
+    process.process_branch. This test will make sure this will never
+    happen again.
+    This time we test a whole import, ie Snappy + Snapcraft twice.
+    '''
+    def runTest(self):
+        db_empty_page_list()
+        self.assertEqual(Page.objects.count(), 0)
+        root = db_create_root_page()
+        snappy_page = db_add_empty_page('Snappy', root)
+        guides = db_add_empty_page('Guides', snappy_page)
+        publish_pages([snappy_page, guides])
+        self.assertEqual(
+            Page.objects.filter(publisher_is_draft=False).count(), 3)
         ExternalDocsBranch.objects.all().delete()
         ExternalDocsBranchImportDirective.objects.all().delete()
         ImportedArticle.objects.all().delete()
